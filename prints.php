df5511e16dcd3723e826d25c33ff7d09d49fe426
robert Salazar <11rsahome@gmail.com>
2019-11-12 16:26:57 +0100

    first commit

---
+++ prints.php
@@ -0,0 +1,14 @@
+<?php
+
+//USO echo
+$mi_primera_variable ="Hola desde php";
+echo $mi_primera_variable.'<br>';
+print("hola Mundo");
+
+$abc = ['texto',1, 2, 3];
+var_dump($abc);
+
+print_r($abc);
+
+
+?>
