05f63edc7dfec5c69199fb93de0f08272ec9d669
robert Salazar <11rsahome@gmail.com>
2019-11-26 17:10:25 +0100

    update conditional and date

---
+++ date/date.php
@@ -0,0 +1,16 @@
+<?php
+//Fecha actual
+echo "La fecha actual".date('d-m-y').'<br>';
+echo date("d").'<br>';
+//dia actual
+echo "Dia actual".date("j").'<br>';
+echo "Dia actual".date("S").'<br>';
+//mes actual
+echo "Mes ACtual".date("F").'<br>';
+//minutos actuales
+echo "Minutos actuales".date("i").'<br>';
+
+echo "La fecha actual".date('Y-m-d').'<br>';
+
+?>
+
